8dcaa2d1417984a99a3ceac1b766c4235458f09d
Zekeriyaev Danil <zekeriyaev_d@auca.kg>
2024-12-26 15:13:41 +0600

    Again clean code

--- src/components/crypto/CryptoList.jsx
+++ src/components/crypto/CryptoList.jsx
@@ -1,4 +1,17 @@
-const sortData = (data, key) => {
-    return [...data].sort((a, b) => a[key] > b[key] ? 1 : -1);
-  };
-  
+import React from "react";
+
+const CryptoList = ({ data }) => {
+  return (
+    <ul>
+      {data.map((coin) => (
+        <li key={coin.id}>
+          <strong>{coin.name} ({coin.symbol})</strong> - ${coin.price_usd} <br />
+          Рейтинг: {coin.rank} | Изменение за 24ч: {coin.percent_change_24h}% | 
+          Капитализация: ${coin.market_cap_usd}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default CryptoList;
--- src/components/crypto/SearchBar.jsx
+++ src/components/crypto/SearchBar.jsx
@@ -1,10 +1,13 @@
+import React from "react";
+
 const SearchBar = ({ onSearch }) => {
-    return (
-      <input
-        type="text"
-        placeholder="Поиск криптовалюты..."
-        onChange={(e) => onSearch(e.target.value)}
-      />
-    );
-  };
-  
+  return (
+    <input
+      type="text"
+      placeholder="Поиск криптовалюты..."
+      onChange={(e) => onSearch(e.target.value)}
+    />
+  );
+};
+
+export default SearchBar;
